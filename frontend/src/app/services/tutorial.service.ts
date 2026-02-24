07800841f9e568eae895edcb676ed4030e581807
Kaushalendra-Marcus <yadavkausha4a5@gmail.com>
2026-02-24 07:57:57 +0000

    fix: missing Tutorial model import

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Tutorial } from '../models/tutorial.model';
 
 const baseUrl = environment.apiUrl;
 
